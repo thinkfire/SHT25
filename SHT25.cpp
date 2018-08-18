88804790d7a4d1fda1d73f8209f7c10af6b92731
Amit <amitate007@gmail.com>
2018-08-18 14:00:51 +0530

    functions added - begin(), resetSensor()

---
+++ SHT25.cpp
@@ -0,0 +1,29 @@
+/*
+  Author : Amit Ate
+  email  : amitate007@gmail.com
+  Description: Sensirion sensor SHT25 is tested with this library on Arduino Uno successefully
+*/
+
+#include<SHT25.h>
+#include<Wire.h>
+
+SHT25::SHT25(){
+  //Do nothing
+}
+
+char SHT25::begin(){
+  Wire.begin();
+  if(resetSensor()){
+    return 1;
+  }else{return 0;}
+}
+
+char SHT25::resetSensor(){
+  Wire.beginTransmission();
+  Wire.write(SOFT_RESET);
+  char error = Wire.endTransmission();
+  if(error == 0){
+    delay(15); //wait for sensor to reset
+    return 1;
+  }else{return 0;}
+}
